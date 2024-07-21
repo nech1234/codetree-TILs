b1fd9589545c51052e57c17087bba631698939ab
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-21 21:53:17 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240721/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+bool inrange(int x, int y, int n1,int n2){
+    return(1<=x&&x<=n1&&1<=y&&y<=n2);
+}
+
+int main() {
+    int n1, n2,x,y;
+    char dir;
+    int dx,dy;
+    cin >> n1 >> n2 >> x >> y >> dir;
+    if(dir == 'U'){
+        dy=0;
+        dx=1;
+    }
+    if(dir == 'D'){
+        dy=0;
+        dx=-1;
+    }
+    if(dir == 'R'){
+        dy=1;
+        dx=0;
+    }
+    if(dir == 'L'){
+        dy=-1;
+        dx=0;
+    }
+    for(int i=0;i<n1;i++){
+        int nx = x+dx;
+        int ny = y+dy;
+        if(inrange(nx,ny,n1,n2)){
+            x=nx;
+            y=ny;
+        }
+        else{
+            dx=-dx;
+            dy=-dy;
+        }
+    }
+    cout << x << ' ' << y;
+    return 0;
+}
