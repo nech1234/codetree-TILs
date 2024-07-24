f2d176f2d2e3cc0fa88fb212264c94abfbee7b2d
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-24 20:58:48 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240724/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+bool inrange(int x, int y, int n, int m) {
+    return(0 <= x && x < m && 0 <= y && y < n);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> arr(n,vector<int>(m,0));
+    int dx[4] = { 0,1,0,-1 };
+    int dy[4] = { 1,0,-1,0 };
+    int dir_num = 0;
+    int x = 0;
+    int y = 0;
+    for (int i = 1; i <= n * m; i++) {
+        arr[x][y] = i;
+        int nx = x + dx[dir_num];
+        int ny = y + dy[dir_num];
+        if (inrange(nx, ny, n, m) && arr[nx][ny] == 0) {
+            x = nx;
+            y = ny;
+        }
+        else {
+            dir_num = (dir_num + 1) % 4;
+            x = x + dx[dir_num];
+            y = y + dy[dir_num];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
