d7f34194ce86f268ec57212c9e4e845c94e85d70
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-24 21:35:56 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/되돌아오기/come-back.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    int dx[4] = {1,-1,0,0};
+    int dy[4] = {0,0,1,-1};
+    int dir_num = 0;
+    int x = 0;
+    int y = 0;
+    int count = 0;
+    for(int i=0;i<N;i++){
+        char c;
+        int n;
+        cin >> c >> n;
+        if(c == 'E'){
+            dir_num = 0;
+        }
+        if(c == 'W'){
+            dir_num = 1;
+        }
+        if(c == 'N'){
+            dir_num = 2;
+        }
+        if(c == 'S'){
+            dir_num = 3;
+        }
+        for(int j=0;j<n;j++){
+            x+=dx[dir_num];
+            y+=dy[dir_num];
+            count++;
+            if(x == 0 && y == 0){
+                cout << count;
+                return 0;
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
