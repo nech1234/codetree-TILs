cfaad267943e199c2e5e895f6aee3f75ca956a53
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-28 03:11:58 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240728/체크판위에서 2/on-the-checkboard-2.cpp
@@ -13,14 +13,16 @@ int main() {
         }
     }
     int x = 0, y = 0, count = 0;
-    for(int i=1;i<r;i++){
-        for(int j=1;j<c;j++){
-            for(int k=i+1;k<r-1;k++){
-                for(int l=j+1;l<r-1;l++){
-                    if(arr[0][0] != arr[i][j] && arr[i][j] != arr[k][l] && arr[k][l] != arr[r-1][c-1]){
+    for(int i = 1; i < r; i++){
+        for(int j = 1; j < c; j++){
+            for(int k = i + 1; k < r - 1; k++){
+                for(int l = j + 1; l < c- 1; l++){
+                     if(arr[0][0] != arr[i][j] && 
+                       arr[i][j] != arr[k][l] &&
+                       arr[k][l] != arr[r - 1][c - 1]){
                         count++;
-                    }
-            }   
+                       }
+                }
             }
         }
     }
