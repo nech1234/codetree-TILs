5cff9e0905e7b76513ac051c286fe43ff4320a03
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-30 22:41:46 +0900

    Title: 황금비율 토스트, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240730/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main() {
+    list<char>li;
+    list<char>::iterator it;
+    int N,C;
+    cin >> N >> C;
+    string s;
+    cin >> s;
+    for(int i=0;i<s.size();i++){
+        li.push_back(s[i]);
+    }
+    it = li.end();
+    for(int i=0;i<C;i++){
+        char c;
+        cin >> c;
+        if(c=='L'){
+            it--;
+        }
+        if(c=='R'){
+            it++;
+        }
+        if(c=='P'){
+            char c;
+            cin >> c;
+            li.insert(it,c);
+        }
+        if(c=='D'){
+            li.erase(it);
+        }
+    }
+    for(it = li.begin();it!=li.end();it++){
+        cout << *it;
+    }
+    return 0;
+}
