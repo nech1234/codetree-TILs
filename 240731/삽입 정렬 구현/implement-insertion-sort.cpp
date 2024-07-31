45f782afc3d6ac8c972775c532e681dc89c994a7
nech1234 <145868124+nech1234@users.noreply.github.com>
2024-07-31 20:44:41 +0900

    Title: 삽입 정렬 구현, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for(int i=1;i<n;i++){
+        int j = i-1;
+        int key = arr[i];
+        while(j>=0&&arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
