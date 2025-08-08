615fbaace4ecc9308756b5c83d4f4313a3cb0d4b
nech1234 <145868124+nech1234@users.noreply.github.com>
2025-08-09 02:14:02 +0900

    Title: 개인정보, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250809/개인정보/personal-info.cpp
+++ 250809/개인정보/personal-info.cpp
@@ -20,13 +20,13 @@ int main() {
     sort(people,people+5);
     cout << "name\n";
     for (int i = 0; i < 5; i++) {
-        cout << get<0>(people[i]) << " " <<get<1>(people[i]) << " " << get<2>(people[i]) << "\n";
+        cout << get<0>(people[i]) << " " <<get<1>(people[i]) << " " <<setprecision(1)<< get<2>(people[i]) <<"\n";
     }
     sort(people,people+5,comp);
     cout << "\n";
     cout << "height\n";
     for (int i = 0; i < 5; i++) {
-        cout << get<0>(people[i]) << " " <<get<1>(people[i]) << " " << get<2>(people[i]) << "\n";
+        cout << get<0>(people[i]) << " " <<get<1>(people[i]) << " " <<setprecision(1)<< get<2>(people[i]) << "\n";
     }
     return 0;
 }
