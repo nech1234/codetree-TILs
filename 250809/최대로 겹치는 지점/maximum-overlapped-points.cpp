a1aa94e9b56a0278685773246c229e2d4ae052b0
nech1234 <145868124+nech1234@users.noreply.github.com>
2025-08-09 22:32:20 +0900

    Title: 최대로 겹치는 지점, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250809/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+
+int main() {
+    cin >> n;
+    int arr[201] = {0};
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+        for(int j = x1[i];j<=x2[i];j++){
+            arr[j]++;
+        }
+    }
+    int result = 0;
+    for(int i=0;i<201;i++){
+        if(result < arr[i]){
+            result = arr[i];
+        }
+    }
+    cout << result;
+
+    // Please write your code here.
+
+    return 0;
+}
