5385d4da7529e5a3c0c999dbcb170e0b05349d0c
nech1234 <145868124+nech1234@users.noreply.github.com>
2025-08-09 21:11:01 +0900

    Title: 요일 맞추기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250809/요일 맞추기/guess-day-of-week.cpp
+++ 250809/요일 맞추기/guess-day-of-week.cpp
@@ -18,12 +18,10 @@ int MonOfDay(int m, int d){
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int total_day = MonOfDay(m2,d2) - MonOfDay(m1,d1) + 1;
-    if(total_day<0){
-        total_day = MonOfDay(m1,d1) - MonOfDay(m2,d2) - 1;
-    }
+    int diff = MonOfDay(m2, d2) - MonOfDay(m1, d1);
+    diff = (diff % 7 + 7) % 7;
 
-    cout << days[total_day%7];
+    cout << days[diff];
 
     return 0;
 }
